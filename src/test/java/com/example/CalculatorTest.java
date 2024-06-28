c8a3dc573bf13245106f54e4f40c451a6bbecb1a
Haythem REHOUMA <rehoumahaythem@gmail.com>
2024-06-27 21:37:55 -0400

    Update CalculatorTest.java

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        Assert.assertEquals(5, calculator.add(2, 3));
+        Assert.assertEquals(6, calculator.add(2, 3));
     }
 
 
